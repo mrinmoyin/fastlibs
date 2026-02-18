86d14c1c810ee41195c5c88a947475bc0adb253c
MRINMOY MONDAL <mm2922894@gmail.com>
2026-02-19 00:11:05 +0530

    Update

--- MPU6050/src/MPU6050.h
+++ MPU6050/src/MPU6050.h
@@ -7,12 +7,6 @@
 #include <SPI.h>
 #include <Wire.h>
 
-#define REG_TEMPERATURE 0x2E
-#define REG_OSS_0       0x34
-#define REG_OSS_1       0x74
-#define REG_OSS_2       0xB4
-#define REG_OSS_3       0xF4
-
 class MPU6050 {
   public:
     MPU6050(
